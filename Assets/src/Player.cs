c2ad9e8ab1c45d6b39b3337bdae59cbab864b5d1
Julian Goldsmith <goldsmithjulian244@googlemail.com>
2020-05-10 18:51:40 +0100

    Added Project

---
+++ Assets/src/Player.cs
@@ -0,0 +1,274 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    GameObject world;
+    World worldScript;
+
+    GameObject cam;
+    Vector3 cameraVelocity = Vector3.zero;
+    public float camSmoothTime = 0.15f;
+    Resolution resolution;
+    public float cameraYOffset = 3;
+
+    public float playerMoveSpeed = 10f;
+    public bool isGrounded = false;
+    public Vector2 velocity;
+    public float moveSmoothTime = 0.1f;
+    public float horizontalVelocity, horizontalVelocityRef;
+    public float gravity = 9.81f;
+    public float maxSlopValue = 70f;
+    public float jumpHeight = 100;
+    Vector2 prevPos;
+
+
+    public float dashSpeed, dashDuration = 0.1f, dashDurationCount = 10f;
+    public bool dashTrigger = false;
+    public AnimationCurve dashCurve;
+
+    public Transform feetTransform;
+    public float stepDistance;
+    public float stickToGroundDistance;
+
+    public Animator anim;
+    public GameObject spriteHolder;
+    Rigidbody2D rb;
+    public Vector2 prevTrackingTransform;
+    public Transform trackingTransform;
+
+    public LayerMask ignorePlayerMask;
+
+    public GameObject followingLight;
+
+    void Start()
+    {
+        cam = Camera.main.gameObject;
+        resolution = Screen.currentResolution;
+        world = GameObject.Find("World");
+        worldScript = world.GetComponent<World>();
+        worldScript.player = this.gameObject;
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        CheckForJump();
+        CheckForDash();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalInput = GetHorizontalInput();
+        PlayerMovement(horizontalInput);
+        CameraControl();
+        LandingCheck();
+        FollowingLight();
+    }
+
+    float GetHorizontalInput()
+    {
+        float rawHorizontalInput = Input.GetAxis("Horizontal");
+        return rawHorizontalInput;
+    }
+
+    void CheckForJump()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            if (isGrounded)
+            {
+                anim.SetTrigger("Jump");
+                anim.SetBool("Grounded", false);
+                velocity.y = jumpHeight;
+                isGrounded = false;
+            }
+        }
+    }
+
+    void CheckForDash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            dashTrigger = true;
+        }
+    }
+
+    void LandingCheck()
+    {
+        if (velocity.y < 0)
+        {
+            Debug.DrawRay(feetTransform.position, Vector2.down * Time.fixedDeltaTime * -velocity.y * 500, Color.white);
+            RaycastHit2D landingRayCheck = Physics2D.Raycast(feetTransform.position, Vector2.down, Time.fixedDeltaTime * -velocity.y * 500);
+            if (landingRayCheck) //checks from inside the player to the ground handles steps
+            {
+                anim.SetBool("Landing", true);
+                Debug.Log("Landing");
+            }
+        }
+    }
+
+    void PlayerMovement(float _horizontalInput)
+    {
+
+        float dashSpeedAtCount = 0;
+        if (dashTrigger)
+        {
+            anim.SetBool("Dash", true);
+            dashTrigger = false;
+            dashDurationCount = 0;
+        }
+        bool isInDash = false;
+        if (dashDurationCount < dashDuration)
+        {
+            isInDash = true;
+            dashDurationCount += Time.fixedDeltaTime;
+            dashSpeedAtCount = dashCurve.Evaluate(dashDurationCount / dashDuration);
+            velocity.y = 0;
+        }
+        else
+        {
+            anim.SetBool("Dash", false);
+        }
+
+
+        //horizontalVelocity = Mathf.SmoothDamp(horizontalVelocity, _horizontalInput, ref horizontalVelocityRef, moveSmoothTime);
+        if (Mathf.Abs(horizontalVelocity) > 0)
+        {
+            horizontalVelocity = (isInDash) ? (horizontalVelocity > 0) ? dashSpeedAtCount : -dashSpeedAtCount : Mathf.Lerp(horizontalVelocity, _horizontalInput, Time.fixedDeltaTime * 200);
+        }
+        else
+        {
+            horizontalVelocity = (isInDash) ? (horizontalVelocity > 0) ? dashSpeedAtCount : -dashSpeedAtCount : Mathf.Lerp(horizontalVelocity, _horizontalInput, Time.fixedDeltaTime * 1000); ;
+        }
+
+
+        Vector2 targetMoveAmount = horizontalVelocity * Vector2.right * Time.fixedDeltaTime * playerMoveSpeed;
+
+        if (trackingTransform != null)
+        {
+            targetMoveAmount += new Vector2(trackingTransform.position.x, trackingTransform.position.y) - prevTrackingTransform;
+        }
+
+
+        targetMoveAmount += velocity;
+        velocity.y -= gravity * Time.fixedDeltaTime;
+
+        bool couldBeGrounded = false;
+        bool couldHitWall = false;
+        Vector2 groundedAt = Vector2.zero;
+
+
+        Vector2 feetPos = new Vector2(feetTransform.position.x, feetTransform.position.y);
+        /*RaycastHit2D wallCheckRay = Physics2D.BoxCast((feetPos + Vector2.up * stepDistance) + Vector2.up * 0.5f, new Vector2(0.001f, 1), 0, targetMoveAmount.normalized, targetMoveAmount.magnitude * 5);
+        if (wallCheckRay)
+        {
+            targetMoveAmount = new Vector2(0, targetMoveAmount.y);
+        }*/
+
+        Vector2 targetFeetPos = new Vector2(feetTransform.position.x, feetTransform.position.y) + targetMoveAmount;
+        Debug.DrawRay(targetFeetPos + Vector2.up * stepDistance, Vector2.down * stepDistance, Color.green);
+        Debug.DrawRay(targetFeetPos, Vector2.down * stickToGroundDistance, Color.red);
+        RaycastHit2D stepCheckRay = Physics2D.Raycast(targetFeetPos + Vector2.up * stepDistance, Vector2.down, stepDistance, ignorePlayerMask);
+        if (stepCheckRay) //checks from inside the player to the ground handles steps
+        {
+            /*float hitAngle = (1 - Vector2.Dot(stepCheckRay.normal, Vector2.up)) * 90;
+            Debug.Log(hitAngle);
+            if (hitAngle < maxSlopValue)
+            {
+                couldBeGrounded = true;
+                groundedAt = stepCheckRay.point;
+            }*/
+            couldBeGrounded = true;
+            groundedAt = stepCheckRay.point;
+            trackingTransform = stepCheckRay.transform;
+        }
+        else
+        {
+            stepCheckRay = Physics2D.Raycast(targetFeetPos, Vector2.down, stickToGroundDistance, ignorePlayerMask);
+            if (stepCheckRay)
+            {
+                /*float hitAngle = (1 - Vector2.Dot(stepCheckRay.normal, Vector2.up)) * 90;
+                if (hitAngle < maxSlopValue)
+                {
+                    anim.SetBool("Grounded", false);
+                    couldBeGrounded = true;
+                    groundedAt = stepCheckRay.point;
+                }*/
+                couldBeGrounded = true;
+                groundedAt = stepCheckRay.point;
+                trackingTransform = stepCheckRay.transform;
+
+            }
+            else
+            {
+                couldBeGrounded = false;
+                trackingTransform = null;
+            }
+        }
+
+        Vector3 moveToPos;
+        if (couldBeGrounded)
+        {
+            anim.SetBool("Landing", false);
+            velocity.y = 0;
+            //rb.MovePosition(groundedAt);
+            moveToPos = groundedAt;
+            anim.SetBool("Grounded", true);
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+            anim.SetBool("Grounded", false);
+            //rb.MovePosition(transform.position + new Vector3((couldHitWall) ? 0 : targetMoveAmount.x, targetMoveAmount.y, 0));
+            moveToPos = transform.position + new Vector3((couldHitWall) ? 0 : targetMoveAmount.x, targetMoveAmount.y, 0);
+        }
+        rb.MovePosition(moveToPos);
+        Vector2 movement = new Vector2(transform.position.x, transform.position.y) - prevPos;
+        if (trackingTransform != null)
+        {
+            movement -= new Vector2(trackingTransform.position.x, trackingTransform.position.y) - prevTrackingTransform;
+        }
+        anim.SetFloat("HorizontalVelocity", Mathf.Abs(movement.x * Time.deltaTime * 250));
+        anim.SetFloat("VerticalVelocity", velocity.y);
+        if (Mathf.Abs(movement.x * Time.deltaTime * 250) > 0f)
+        {
+
+            transform.localScale = new Vector3((_horizontalInput == 0) ? transform.localScale.x : (_horizontalInput > 0) ? 1 : -1, transform.localScale.y, transform.localScale.z);
+        }
+
+        spriteHolder.transform.eulerAngles = new Vector3(spriteHolder.transform.rotation.eulerAngles.x, spriteHolder.transform.rotation.eulerAngles.y, movement.x * Time.deltaTime * 250 * -9f);
+        prevPos = transform.position;
+        if (trackingTransform != null)
+        {
+            prevTrackingTransform = trackingTransform.position;
+        }
+
+    }
+
+    void CameraControl()
+    {
+        if (resolution.width != Screen.width || resolution.height != Screen.height)
+        {
+            worldScript.CalculateLevelConstaints();
+            resolution = Screen.currentResolution;
+        }
+        WorldConstrints currentConstaints = worldScript.worldConstraints;
+        Vector3 targetPos = new Vector3(this.transform.position.x, this.transform.position.y + cameraYOffset, -10);
+        targetPos.x = Mathf.Clamp(targetPos.x, currentConstaints.minX, currentConstaints.maxX);
+        targetPos.y = Mathf.Clamp(targetPos.y, currentConstaints.minY, currentConstaints.maxY);
+        cam.transform.position = Vector3.SmoothDamp(cam.transform.position, targetPos, ref cameraVelocity, camSmoothTime);
+
+    }
+
+    void FollowingLight()
+    {
+        followingLight.transform.position = Vector3.Lerp(followingLight.transform.position, transform.position - Vector3.forward * 3 + Vector3.up * 2 + Vector3.right * 1.5f, 3f * Time.deltaTime);
+    }
+
+
+}
---
+++ Assets/src/World.cs
@@ -0,0 +1,224 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+
+
+
+    [Header("Background Setup")]
+    public List<GameObject> levelBoundsQuad;
+    public List<Vector3> parallaxReferencePositions = new List<Vector3>();
+    public List<BackgroundLayer> backgroundLayers = new List<BackgroundLayer>();
+    public List<ParallaxCloud> parallaxClouds = new List<ParallaxCloud>();
+
+    [Header("Background Setup")]
+    public List<MovingPlatform> movingPlatfomrs = new List<MovingPlatform>();
+    [HideInInspector]
+    public WorldConstrints worldConstraints;
+
+    Transform cam;
+    Vector3 cameraPrevPos;
+
+    [Header("Positional Triggers")]
+    public List<PositionTrigger> positionTriggers = new List<PositionTrigger>();
+
+    [HideInInspector]
+    public int parallaxRefPos;
+
+    [HideInInspector]
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        cam = Camera.main.transform;
+        cameraPrevPos = cam.position;
+        //parallaxReferencePos = cam.position;
+        CalculateLevelConstaints();
+        CalculateLayerParallexOffsets();
+    }
+
+
+    void FixedUpdate()
+    {
+        CheckPositionalTriggers();
+        CalculateLevelConstaints();
+        MovingPlatforms();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        BackgroundParallax();
+    }
+
+    void BackgroundParallax()
+    {
+        foreach (BackgroundLayer layer in backgroundLayers)
+        {
+            /*float parallexX = (cameraPrevPos.x - cam.transform.position.x) * -layer.distance / 100f;
+            float parallexY = (cameraPrevPos.y - cam.transform.position.y) * -layer.distance / 100f;
+            Vector3 targetPos = new Vector3(layer.layerObject.transform.position.x + parallexX, layer.layerObject.transform.position.y + parallexY, layer.layerObject.transform.position.z);
+            layer.layerObject.transform.position = targetPos;//Vector3.Lerp(layer.layerObject.transform.position, targetPos, parallaxSmoothing * Time.deltaTime);
+            */
+            float parallexX = (parallaxReferencePositions[parallaxRefPos].x - cam.transform.position.x) * -layer.distance / 100f;
+            float parallexY = (parallaxReferencePositions[parallaxRefPos].y - cam.transform.position.y) * -layer.distance / 100f;
+            Vector3 targetPos = new Vector3(layer.layerStartingPos.x + parallexX, layer.layerStartingPos.y + parallexY, layer.layerObject.transform.position.z);
+            layer.layerObject.transform.position = targetPos;
+        }
+        foreach (ParallaxCloud cloud in parallaxClouds)
+        {
+            float parallexX = (cameraPrevPos.x - cam.transform.position.x) * -cloud.distance / 100f;
+            float parallexY = (cameraPrevPos.y - cam.transform.position.y) * -cloud.distance / 100f;
+            Vector3 targetPos = new Vector3(cloud.layerObject.transform.position.x + parallexX, cloud.layerObject.transform.position.y + parallexY, cloud.layerObject.transform.position.z);
+            cloud.layerObject.transform.position = targetPos;//Vector3.Lerp(layer.layerObject.transform.position, targetPos, parallaxSmoothing * Time.deltaTime);
+            cloud.layerObject.transform.position += Vector3.right * cloud.speed * Time.deltaTime * 0.1f;
+
+            if (cloud.layerObject.transform.position.x > cam.position.x + (Camera.main.orthographicSize * Camera.main.aspect + 10))
+            {
+                cloud.layerObject.transform.position = new Vector3(cam.position.x - (Camera.main.orthographicSize * Camera.main.aspect + 3),
+                            cloud.layerObject.transform.position.y, cloud.layerObject.transform.position.z);
+            }
+            if (cloud.layerObject.transform.position.x < cam.position.x - (Camera.main.orthographicSize * Camera.main.aspect + 10))
+            {
+                cloud.layerObject.transform.position = new Vector3(cam.position.x + (Camera.main.orthographicSize * Camera.main.aspect + 3),
+                            cloud.layerObject.transform.position.y, cloud.layerObject.transform.position.z);
+            }
+        }
+        cameraPrevPos = cam.position;
+    }
+
+    void CalculateLayerParallexOffsets()
+    {
+        for (int i = 0; i < backgroundLayers.Count; i++)
+        {
+            backgroundLayers[i].layerStartingPos = backgroundLayers[i].layerObject.transform.position;
+        }
+    }
+
+    public void CalculateLevelConstaints()
+    {
+        Camera camera = cam.GetComponent<Camera>();
+        worldConstraints.minX = levelBoundsQuad[parallaxRefPos].transform.position.x - (levelBoundsQuad[parallaxRefPos].transform.localScale.x / 2) + camera.orthographicSize * camera.aspect;
+        worldConstraints.maxX = levelBoundsQuad[parallaxRefPos].transform.position.x + (levelBoundsQuad[parallaxRefPos].transform.localScale.x / 2) - camera.orthographicSize * camera.aspect;
+        worldConstraints.minY = levelBoundsQuad[parallaxRefPos].transform.position.y - (levelBoundsQuad[parallaxRefPos].transform.localScale.y / 2) + camera.orthographicSize;
+        worldConstraints.maxY = levelBoundsQuad[parallaxRefPos].transform.position.y + (levelBoundsQuad[parallaxRefPos].transform.localScale.y / 2) - camera.orthographicSize; ;
+    }
+
+    void MovingPlatforms()
+    {
+        for (int i = 0; i < movingPlatfomrs.Count; i++)
+        {
+            MovingPlatform mp = movingPlatfomrs[i];
+            //mp.platformObject.transform.position = Vector3.SmoothDamp(mp.platformObject.transform.position, mp.wayPoints[mp.targetIndex].position, ref mp.smoothVelocity, mp.smoothTime);
+            mp.platformObject.transform.position = mp.platformObject.transform.position + (mp.wayPoints[mp.targetIndex].position - mp.platformObject.transform.position).normalized * mp.speed * Time.fixedDeltaTime;
+            if ((mp.platformObject.transform.position - mp.wayPoints[mp.targetIndex].position).magnitude < 0.1f)
+            {
+                if (mp.targetIndex != mp.wayPoints.Length - 1)
+                {
+                    mp.targetIndex++;
+                }
+                else
+                {
+                    mp.targetIndex = 0;
+                }
+
+            }
+        }
+    }
+
+    void CheckPositionalTriggers()
+    {
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        for (int i = 0; i < positionTriggers.Count; i++)
+        {
+            PositionTrigger pt = positionTriggers[i];
+            bool reject = false;
+            foreach (Argument arg in pt.conditions)
+            {
+                switch (arg.argument)
+                {
+                    case ArgumentType.XLessThan:
+                        reject = !(playerPos.x < arg.value);
+                        break;
+                    case ArgumentType.XGreaterThan:
+                        reject = !(playerPos.x > arg.value);
+                        break;
+                    case ArgumentType.XEqualToo:
+                        reject = !(playerPos.x == arg.value);
+                        break;
+                    case ArgumentType.YLessThan:
+                        reject = !(playerPos.y < arg.value);
+                        break;
+                    case ArgumentType.YGreaterThan:
+                        reject = !(playerPos.y > arg.value);
+                        break;
+                    case ArgumentType.YEqualToo:
+                        reject = !(playerPos.y == arg.value);
+                        break;
+                }
+            }
+            if (!reject)
+            {
+                parallaxRefPos = pt.targetParallaxRefPos;
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class BackgroundLayer
+{
+    [Range(0, 100)]
+    public float distance;
+    public GameObject layerObject;
+    public Vector3 layerStartingPos;
+}
+
+[System.Serializable]
+public class ParallaxCloud
+{
+    [Range(0, 100)]
+    public float distance;
+    public GameObject layerObject;
+    public float speed;
+    [HideInInspector]
+    public Vector3 layerStartingPos;
+}
+
+public struct WorldConstrints
+{
+    public float minX, maxX, minY, maxY;
+}
+
+[System.Serializable]
+public class MovingPlatform
+{
+    public GameObject platformObject;
+    public Transform[] wayPoints;
+    public float speed = 3;
+    public Vector3 smoothVelocity;
+    public int targetIndex;
+}
+
+[System.Serializable]
+public class PositionTrigger
+{
+    public List<Argument> conditions;
+    public int targetParallaxRefPos = 1;
+}
+
+[System.Serializable]
+public class Argument
+{
+    public float value;
+    public ArgumentType argument;
+}
+
+public enum ArgumentType
+{
+    XGreaterThan, XLessThan, XEqualToo, YGreaterThan, YLessThan, YEqualToo
+}
